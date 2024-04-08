730cc2e3be5c0733c1e2e149e8ac6521c0a6f0ae
Dinithi Sanjana <IT22074072@my.sliit.lk>
2024-04-08 10:46:14 +0530

    add some dummy data in the Main Activity2

--- app/src/main/java/com/example/recycler_view_tute6/MainActivity2.kt
+++ app/src/main/java/com/example/recycler_view_tute6/MainActivity2.kt
@@ -5,8 +5,26 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.recycler_view_tute6.models.Post
 
 class MainActivity2 : AppCompatActivity() {
+
+    private val postsList = mutableListOf<Post>()
+    init{
+        postsList.add(Post("Hannah Ice", "Anyone else here from New York?", 62))
+        postsList.add(Post("Jane Smith", "Sunsets are so beautiful.", 37))
+        postsList.add(Post("George Hill", "Anyone else here from New York?", 32))
+        postsList.add(Post("Hannah Ice", "At the beach with friends.", 32))
+        postsList.add(Post("Charlie Davis", "Sunsets are so beautiful.", 57))
+        postsList.add(Post("Eddie Foster", "At the beach with friends.", 64))
+        postsList.add(Post("Bob Brown", "This is my first post on Android Social Media app", 19))
+        postsList.add(Post("John Doe", "Loving the new app update!", 90))
+        postsList.add(Post("Fay Green", "Look at my new dog!", 63))
+        postsList.add(Post("Diana Evans", "Anyone else here from New York?", 38))
+
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
